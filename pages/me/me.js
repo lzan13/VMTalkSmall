a1e96a44f6b1f9fbabba1867a5f643efd93814c5
liulizheng <liulizheng@weidian.com>
2018-12-10 18:45:32 +0800

    首次提交 添加我的见面，开始尝试编写我的第一个小程序

---
+++ pages/me/me.js
@@ -0,0 +1,105 @@
+// pages/me/me.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    user: {},
+    isLoadUser: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    if (app.common.user) {
+      // 已经有用户信息直接使用
+      this.setData({
+        user: app.common.user,
+        isLoadUser: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回，所以此处加入 callback 以防止这种情况
+      app.userReadyCallback = res => {
+        this.setData({
+          user: res.userInfo,
+          isLoadUser: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.common.user = res.userInfo
+          this.setData({
+            user: res.userInfo,
+            isLoadUser: true
+          })
+        }
+      })
+    }
+  },
+  /**
+   * 自定义方法，获取用户信息
+   */
+  getUserInfo: function(e) {
+    console.log(e)
+    app.common.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      isLoadUser: true
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
