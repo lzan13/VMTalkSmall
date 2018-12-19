cb5eb5538bf567d145f374960b7633f0b176effa
liulizheng <liulizheng@weidian.com>
2018-12-19 19:03:56 +0800

    添加触摸移动方法

--- pages/components/refresh-view/index.js
+++ pages/components/refresh-view/index.js
@@ -6,17 +6,17 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    
+
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    sstatus: 1,// 1是滑动到顶部 2是滑动中中 3是滑动到底部
-    isRefresh: false,//是否显示刷新头
-    isLoadMore: false,//加载更多
-    clientY: 0,//Y方向手指按下的方向
+    scrollStatus: 0, // 0-滚动到顶部 1-滚动到中间 2-滚动到底部
+    isRefresh: false, // 是否显示刷新图标
+    isLoadMore: false, // 是否显示加载更多
+    clientY: 0, // Y方向手指按下的方向
   },
 
   /**
@@ -24,32 +24,34 @@ Component({
    */
   methods: {
 
-    //滑动到顶端
-    _onScrollTop: function (e) {
+    /**
+     * 滑动到顶端
+     */
+    _onScrollTop: function(e) {
       this.setData({
-        sstatus: 1
+        scrollStatus: 0
       })
-    },
-    //滑动到底部
-    _onScrollBottom: function (e) {
-      this.setData({
-        sstatus: 3
-      })
-
     },
     /**
      * 滑动中
      */
     _onScroll: function (e) {
       this.setData({
-        sstatus: 2,
+        scrollStatus: 1,
       })
-
     },
     /**
-     * 手指按下
+     * 滑动到底部
      */
-    _onTouchStart: function (e) {
+    _onScrollBottom: function(e) {
+      this.setData({
+        scrollStatus: 2
+      })
+    },
+    /**
+     * 触摸开始
+     */
+    _onTouchStart: function(e) {
       console.log(e)
       var touchPoint = e.touches[0];
       var clientY = touchPoint.clientY
@@ -58,16 +60,22 @@ Component({
       })
     },
     /**
-    * 抬起手指
-    */
-    _onTouchEnd: function (e) {
+     * 触摸移动
+     */
+    _onTouchMove:function(){
+
+    },
+    /**
+     * 触摸结束
+     */
+    _onTouchEnd: function(e) {
       console.log(e)
       var context = this
       var upPoint = e.changedTouches[0];
       var endY = upPoint.clientY
       var pointTopointY = Math.abs(endY - this.data.clientY)
-      var status = this.data.sstatus
-      vlog.i("滑动的距离:" + pointTopointY + "----:当前的状态:" + status)
+      var status = this.data.scrollStatus
+      vlog.i("滚动的距离:" + pointTopointY + "----:当前的状态:" + status)
       //上拉刷新
       if (status == 1 && pointTopointY > 50) {
         this.setData({
@@ -81,10 +89,10 @@ Component({
         })
       }
       //两秒后隐藏加载条条
-      setTimeout(function () {
+      setTimeout(function() {
         context.setData({
-          isRefresh: false,//是否显示刷新头
-          isLoadMore: false,//加载更多
+          isRefresh: false, //是否显示刷新头
+          isLoadMore: false, //加载更多
         })
       }, 3000)
     },
