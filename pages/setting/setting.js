70fa49ecc8112931d455a0e06f490f34c656e26d
liulizheng <liulizheng@weidian.com>
2018-12-28 19:09:05 +0800

    修改设置界面

--- pages/setting/setting.js
+++ pages/setting/setting.js
@@ -10,6 +10,39 @@ Page({
   data: {
     sbHeight: 0,
     navHeight: 0,
+    objectArray: [{
+        id: '0',
+        name: '随机'
+      }, {
+        id: 'a',
+        name: '动画'
+      },
+      {
+        id: 'b',
+        name: '漫画'
+      },
+      {
+        id: 'c',
+        name: '游戏'
+      },
+      {
+        id: 'd',
+        name: '小说'
+      },
+      {
+        id: 'e',
+        name: '原创'
+      },
+      {
+        id: 'f',
+        name: '网络'
+      },
+      {
+        id: 'g',
+        name: '其他'
+      }
+    ],
+    index: 0,
   },
 
   /**
@@ -22,39 +55,11 @@ Page({
       navHeight: app.common.navHeight
     });
   },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
+  bindPickerChange(e) {
+    this.setData({
+      index: e.detail.value
+    })
   },
 
 })
